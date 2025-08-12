faa35c38e095010c95dd1865361a6e8838f45b4a
rodofrodo <rodofrodo708@gmail.com>
2025-08-12 20:07:14 +0200

    Added new color conversion files

---
+++ CMYK.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorPick
+{
+    public struct CMYK
+    {
+        public double C { get; set; }
+        public double M { get; set; }
+        public double Y { get; set; }
+        public double K { get; set; }
+        public CMYK(double c, double m, double y, double k)
+        {
+            C = c;
+            M = m;
+            Y = y;
+            K = k;
+        }
+    }
+}
